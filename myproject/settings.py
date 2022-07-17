55c085e3d6f727b7af1ab1d0db522d14ef9d8c60
Bartek1995 <bartek.swatek@wp.pl>
2022-07-17 15:54:04 +0200

    Set database connection

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'find_your_garage_db',
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASSWORD'),
+        'HOST': os.environ.get('DBENDPOINT'),
+        'PORT': os.environ.get('DBPORT'),
     }
 }
 
