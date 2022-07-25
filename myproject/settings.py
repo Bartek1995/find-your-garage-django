31f2f435d8d947ce0c726ad7e02c17de2e51d7e6
Bartek1995 <bartek.swatek@wp.pl>
2022-07-25 11:45:29 +0200

    Add WhiteNoise to server staticfiles in Heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -153,6 +154,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for myproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
+from whitenoise import WhiteNoise
 
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 application = get_wsgi_application()
+application = WhiteNoise(application, root=BASE_DIR / 'staticfiles')
