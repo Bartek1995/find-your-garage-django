ddcf208228d2601b2bbb20843779b43ab4be3416
Bartek1995 <bartek.swatek@wp.pl>
2022-07-25 11:05:48 +0200

    Rename application

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FindYourGarageConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'find_your_garage'
+    name = 'main'
