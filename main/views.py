cee818e8e4d33dc427835291c6f37acb6393be76
Bartek1995 <bartek.swatek@wp.pl>
2022-08-14 17:00:41 +0200

    Create: - Dashboard view in main application - All required css/scss files - All html template files

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class MainPage(TemplateView):
     template_name = 'main/index.html'
+
+
+class Dashboard(TemplateView, LoginRequiredMixin):
+    template_name = 'main/dashboard.html'
