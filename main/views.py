31bbbd41d3a8e68d1f30c18532dd6b6d6a2d12f1
Bartek1995 <bartek.swatek@wp.pl>
2022-09-29 18:05:51 +0200

    Add conditions and box if user doesn't have garage

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
+from garages.models import Garage
 
 
 class MainPage(TemplateView):
@@ -10,11 +11,18 @@ class MainPage(TemplateView):
 
 class Dashboard(TemplateView, LoginRequiredMixin):
 
-    def get_context_data(self, **kwargs):
+    def get(self, *args, **kwargs):
         if self.request.user.is_customer:
             self.template_name = 'main/dashboard_customer.html'
 
         elif self.request.user.is_entrepreneur:
             self.template_name = 'main/dashboard_entrepreneur.html'
-        return super().get_context_data(**kwargs)
+        return super().get(*args, **kwargs)
 
+    def get_context_data(self, **kwargs):
+        if self.request.user.is_entrepreneur:
+            try:
+                user_garage = Garage.objects.get(user=self.request.user)
+            except Garage.DoesNotExist:
+                user_garage = None
+        return super().get_context_data(**kwargs)
