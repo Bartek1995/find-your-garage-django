4410aa73827ce548364555d0e5b1692b605ea933
Bartek1995 <bartek.swatek@wp.pl>
2022-10-12 01:33:22 +0200

    Add message after successfully creating garage

--- garages/views.py
+++ garages/views.py
@@ -1,13 +1,15 @@
 from django.views.generic.edit import CreateView
-from .forms import GarageForm
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import GarageForm
 
 
 class GarageCreateView(LoginRequiredMixin, CreateView):
     template_name = 'garages/garage_create.html'
     form_class = GarageForm
-    success_url = '/garage/create_complete'
-    
+    success_url = '/dashboard'
+
     def form_valid(self, form):
         form.instance.user_id = self.request.user.id
+        self.request.messages = messages.success(self.request, 'Warsztat został pomyślnie utworzony.')
         return super().form_valid(form)
