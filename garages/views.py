daa9e4bd9c9aeaf1e3497f72911191bb6fb23456
Bartek1995 <bartek.swatek@wp.pl>
2022-10-12 16:58:25 +0200

    Add securing conditions in "garage create view"
    I implemented 'allowed_users' decorator to ensure that loged in user is in selected group
    
    Additional I set 'UserPassTestMixin' to be sure that user cannot create a second garage

--- garages/views.py
+++ garages/views.py
@@ -1,15 +1,34 @@
 from django.views.generic.edit import CreateView
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.utils.decorators import method_decorator
+from django.shortcuts import redirect
+
+from .decorators import allowed_users
 from .forms import GarageForm
+from .models import Garage
 
 
-class GarageCreateView(LoginRequiredMixin, CreateView):
+@method_decorator(allowed_users(allowed_groups=['Entrepreneur']), name='dispatch')
+class GarageCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     template_name = 'garages/garage_create.html'
     form_class = GarageForm
     success_url = '/dashboard'
 
+    def test_func(self):
+        try:
+            is_garage_exist = Garage.objects.get(user=self.request.user)
+        except Garage.DoesNotExist:
+            is_garage_exist = False
+        return not(is_garage_exist)
+
+    def handle_no_permission(self):
+        self.request.messages = messages.warning(
+            self.request, message='Istnieje już zarejestrowany warsztat na to konto użytkownika')
+        return redirect('/dashboard')
+
     def form_valid(self, form):
         form.instance.user_id = self.request.user.id
-        self.request.messages = messages.success(self.request, 'Warsztat został pomyślnie utworzony.')
+        self.request.messages = messages.success(
+            self.request, 'Warsztat został pomyślnie utworzony.')
         return super().form_valid(form)
