41c5dfb83fef6c010e7c23a6c0dee0c92f400775
Bartek1995 <bartek.swatek@wp.pl>
2022-10-26 17:41:31 +0200

    Add additional button to reset form:
    - Created additional a button with reload page function
    - Override "form_invalid" method in view to send notification to user

--- accounts/views.py
+++ accounts/views.py
@@ -22,3 +22,7 @@ class ProfileEditView(UpdateView):
     def form_valid(self, form):
         messages.success(self.request, "Dane zostały zaktualizowane")
         return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.warning(self.request, "Popraw dane w formularzu aby zatwierdzić zmiany")
+        return super().form_invalid(form)
