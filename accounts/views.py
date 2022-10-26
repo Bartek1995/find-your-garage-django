7a5e9dab5e636094acbffb51738379a6607c34ba
Bartek1995 <bartek.swatek@wp.pl>
2022-10-26 17:26:31 +0200

    Add profile edit feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic.edit import UpdateView
+from .forms import ProfileEditForm
+from .models import CustomUser
 
-# Create your views here.
+
+class ProfileEditView(UpdateView):
+    model = CustomUser
+    form_class = ProfileEditForm
+    template_name = 'account/profile_edit.html'
+    success_url = "/dashboard"
+
+    def get(self, request, *args, **kwargs):
+        form = self.get_form(self.form_class)
+        form.fields['first_name'].initial = self.request.user.first_name
+        form.fields['last_name'].initial = self.request.user.last_name
+        return super().get(self, request, *args, **kwargs)
+
+    def get_object(self):
+        return self.request.user
+
+    def form_valid(self, form):
+        messages.success(self.request, "Dane zostały zaktualizowane")
+        return super().form_valid(form)
