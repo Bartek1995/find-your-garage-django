5b02f3a5410c159b382a8ec6a95405bd9c81ffb5
Bartek1995 <bartek.swatek@wp.pl>
2022-10-29 01:30:01 +0200

    Add custom storage to model field

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 from address.models import AddressField
 
+from myproject.azure_blob_storage import OverwriteStorage
+
 
 class CustomUser(AbstractUser):
     is_customer = models.BooleanField(default=False)
     is_entrepreneur = models.BooleanField(default=False)
     address = AddressField(blank=True, null=True)
+    avatar = models.ImageField(blank=True, null=True, storage=OverwriteStorage())
