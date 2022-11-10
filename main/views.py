def90e2561155c9466bb3b2992ff9eb5baca56c8
Bartek1995 <bartek.swatek@wp.pl>
2022-11-11 00:10:22 +0100

    Modify the customer panel view:
    - View - sending information about all cars registered on the account of the logged in user
    - Mock order cost data to display the generated charts
    - Added ability to switch pages of the main panel to view information about different cars has been added

--- main/views.py
+++ main/views.py
@@ -1,14 +1,21 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from django.views.decorators.gzip import gzip_page
 from django.utils.decorators import method_decorator
+from django.core import serializers
 
 from garages.models import Garage
+from cars.models import Car
+
+from json import dumps
 
 
 @method_decorator(gzip_page, name='dispatch')
 class MainPage(TemplateView):
+    """
+    Main page view, which is displayed as the main page, when user visits the website.
+    """
+    
     template_name = 'main/index.html'
 
     def dispatch(self, request, *args, **kwargs):
@@ -16,7 +23,10 @@ class MainPage(TemplateView):
 
 
 class Dashboard(LoginRequiredMixin, TemplateView):
-
+    """
+    Based on user type dashboard view, it returns different template.
+    """
+    
     def get(self, *args, **kwargs):
         if self.request.user.is_customer:
             self.template_name = 'main/dashboard_customer.html'
@@ -27,9 +37,30 @@ class Dashboard(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Dashboard, self).get_context_data(**kwargs)
+        
         if self.request.user.is_entrepreneur:
             try:
                 context['user_garage'] = Garage.objects.get(user=self.request.user)
             except Garage.DoesNotExist:
                 context['user_garage'] = None
+                
+        elif self.request.user.is_customer:
+            mocked_data = []
+            try:
+                context['user_cars'] = Car.objects.filter(user=self.request.user)
+                if not context['user_cars']:
+                    raise Car.DoesNotExist
+                for car in context['user_cars']:
+                    # MOCKING CARS COSTS TO CONSTRUCT CHARTS - IT'S TEMPORARY SOLUTION
+                    car.mock_information_about_orders()
+                    car_as_dict = car.__dict__
+                    del car_as_dict['_state']
+                    mocked_data.append(car_as_dict)
+                
+                dataJSON = dumps(mocked_data)
+                context['user_cars_json'] = dataJSON
+                
+            except Car.DoesNotExist:
+                context['user_cars'] = None
+                
         return context
