45784dbaab5e229f6c43cc1713464cfa0e933e3d
Bartek1995 <bartek.swatek@wp.pl>
2022-11-12 13:28:40 +0100

    Fix problem with cleaning 'vin_number':
    - After successfully update car model ,  a value inside 'vin_number' field has been cleaned, it's happend becouse 'vin_number' field occured in list of fields to update.

--- cars/views.py
+++ cars/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.urls import reverse_lazy
-from .models import Car
 import requests
 
+from .models import Car
 from accounts.mixins import GroupRequiredMixin
 
 
@@ -60,6 +60,9 @@ class CarCreateViewVinDecoding(LoginRequiredMixin, GroupRequiredMixin, CreateVie
 
 
 class CarCreateViewManual(LoginRequiredMixin, GroupRequiredMixin, CreateView):
+    """
+    View used for creating car manually
+    """
     model = Car
     fields = ['brand', 'model', 'vin_number', 'production_year', 'engine_capacity',
               'gearbox_type', 'engine_type', 'engine_code', 'engine_power', 'body_type', ]
@@ -75,10 +78,12 @@ class CarCreateViewManual(LoginRequiredMixin, GroupRequiredMixin, CreateView):
 
 
 class CarEditView(LoginRequiredMixin, GroupRequiredMixin, UpdateView):
-    
+    """
+    View used for editing car.
+    """
     model = Car
-    fields = ['brand', 'model', 'vin_number', 'production_year', 'engine_capacity',
-              'gearbox_type', 'engine_type', 'engine_code', 'engine_power', 'body_type', ]
+    fields = ['brand', 'model', 'production_year', 'engine_capacity',
+              'gearbox_type', 'engine_type', 'engine_code', 'engine_power', 'body_type']
     template_name = 'cars/car_edit.html'
     success_url = reverse_lazy('car_list')
     required_group = "Customer"
@@ -87,15 +92,16 @@ class CarEditView(LoginRequiredMixin, GroupRequiredMixin, UpdateView):
         try:
             car = self.get_object()
         except Car.DoesNotExist:
-            self.messages = messages.error(self.request, "Nie znaleziono pojazdu o podanym ID.")
+            self.messages = messages.error(
+                self.request, "Nie znaleziono pojazdu o podanym ID.")
             return redirect('/dashboard')
-        
         if self.request.user.id == car.user_id:
             return super().get(request, *args, **kwargs)
         else:
-            self.messages = messages.error(self.request, "Nie masz uprawnień do edycji tego pojazdu.")
+            self.messages = messages.error(
+                self.request, "Nie masz uprawnień do edycji tego pojazdu.")
             return redirect('/dashboard')
-        
+
     def form_valid(self, form):
         form.instance.user_id = self.request.user.id
         self.request.messages = messages.success(
@@ -104,15 +110,18 @@ class CarEditView(LoginRequiredMixin, GroupRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return Car.objects.get(id=self.kwargs['car_id'])
-    
-    
+
+
 class CarListView(LoginRequiredMixin, GroupRequiredMixin, ListView):
+    """
+    View used for listing cars of logged user.
+    """
     template_name = 'cars/car_list.html'
     required_group = "Customer"
-    
+
     def get_queryset(self):
         return Car.objects.filter(user_id=self.request.user.id)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['cars'] = self.get_queryset()
@@ -120,27 +129,34 @@ class CarListView(LoginRequiredMixin, GroupRequiredMixin, ListView):
 
 
 class CarDeleteView(LoginRequiredMixin, GroupRequiredMixin, DeleteView):
+    """
+    View used for deleting car. User can delete only his own cars. It's impossible to delete car by entering car_id in URL. User must use delete button on car_list page and send POST request.
+    """
     model = Car
     required_group = "Customer"
     success_url = '/dashboard'
-    
+
     def get(self, request, *args, **kwargs):
-        self.messages = messages.error(self.request, "Skorzystaj z przycisku usuń, korzystając z funkcji wyświetlenia listy pojazdów, aby usunąć pojazd.")
+        self.messages = messages.error(
+            self.request, "Skorzystaj z przycisku usuń, korzystając z funkcji wyświetlenia listy pojazdów, aby usunąć pojazd.")
         return redirect('/dashboard')
-    
+
     def post(self, *args, **kwargs):
         try:
             car = self.get_object()
         except Car.DoesNotExist:
-            self.messages = messages.error(self.request, "Nie znaleziono pojazdu o podanym ID.")
+            self.messages = messages.error(
+                self.request, "Nie znaleziono pojazdu o podanym ID.")
             return redirect('/dashboard')
-        
+
         if self.request.user.id == car.user_id:
-            self.messages = messages.success(self.request, "Pojazd został usunięty")
+            self.messages = messages.success(
+                self.request, "Pojazd został usunięty")
             return super().post(*args, **kwargs)
         else:
-            self.messages = messages.error(self.request, "Nie masz uprawnień do usunięcia tego pojazdu.")
+            self.messages = messages.error(
+                self.request, "Nie masz uprawnień do usunięcia tego pojazdu.")
             return redirect('/dashboard')
-        
+
     def get_object(self, queryset=None):
         return Car.objects.get(id=self.kwargs['car_id'])
