c9721d3afb73986a80a4b24bf758de54bcda2fae
Bartek1995 <bartek.swatek@wp.pl>
2022-11-14 11:38:04 +0100

    Fix bug with validator, missing 'not' keyword

--- garages/validators.py
+++ garages/validators.py
@@ -2,6 +2,6 @@ from django.core.exceptions import ValidationError
 
 
 def validate_poland_country(word):
-    if 'Poland' in word:
+    if 'Poland' not in word:
         raise ValidationError(
             "Znacznik warsztatu powinien znajdować sie na terytorium Polski.")
