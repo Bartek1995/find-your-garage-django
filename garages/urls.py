fc5cd463be3fdcefe8d4a2777bf22b2d45c37506
Bartek1995 <bartek.swatek@wp.pl>
2022-11-17 03:00:57 +0100

    Add garage edit url

--- garages/urls.py
+++ garages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GarageCreateView
-from django.contrib.auth.decorators import login_required
+from .views import GarageCreateView, GarageEditView
+
 
 urlpatterns = [
     path('create/', GarageCreateView.as_view(), name='garage_create'),
+    path('edit/<int:garage_id>', GarageEditView.as_view(), name='garage_edit'),
 ]
