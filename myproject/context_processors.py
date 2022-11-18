13f559c50fb16e512e0dd8a63dccef8a236a877f
Bartek1995 <bartek.swatek@wp.pl>
2022-11-18 03:07:03 +0100

    Fix problem with index out of range:
    - Modified conditions of function

--- myproject/context_processors.py
+++ myproject/context_processors.py
@@ -2,14 +2,15 @@ from garages.models import Garage
 
 
 def user_garage(request):
-    user_group = request.user.groups.all()[0].name
-    if user_group == 'Entrepreneur':
-        if request.user.is_authenticated:
-            try:
-                garage = Garage.objects.get(user=request.user)
-            except Garage.DoesNotExist:
-                return {'user_garage': None}
-            else:
-                return {'user_garage': garage}
-        else:
-            return {'user_garage': None}
+    if request.user.groups.exists():
+        group = request.user.groups.all()
+        for key in group:
+            if key.name == 'Entrepreneur':
+                try:
+                    garage = Garage.objects.get(user=request.user)
+                except Garage.DoesNotExist:
+                    return {'user_garage': None}
+                else:
+                    return {'user_garage': garage}
+    else:
+        return {'user_garage': None}
