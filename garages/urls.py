c95060fc57484e871630b618d25770c40f61b7b0
Bartek1995 <bartek.swatek@wp.pl>
2022-11-20 23:01:46 +0100

    Add garage information url

--- garages/urls.py
+++ garages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GarageCreateView, GarageEditView, GarageDeleteView
+from .views import GarageCreateView, GarageEditView, GarageDeleteView, GarageInformationView
 
 
 urlpatterns = [
     path('create/', GarageCreateView.as_view(), name='garage_create'),
     path('edit/<int:garage_id>', GarageEditView.as_view(), name='garage_edit'),
     path('delete/<int:garage_id>', GarageDeleteView.as_view(), name='garage_delete'),
+    path('information/<int:pk>', GarageInformationView.as_view(), name='garage_information'),
 ]
