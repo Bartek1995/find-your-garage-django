a33a2402853917c959af911a6af75d54e46ae8e0
Bartek1995 <bartek.swatek@wp.pl>
2022-11-22 19:31:36 +0100

    Fix problem with InitMap is not a function" error

--- static/js/initialize_garage_information_map.js
+++ static/js/initialize_garage_information_map.js
@@ -2,22 +2,22 @@ const mapDiv = document.querySelector('#map');
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    let map;
 
-    const myLatLng = { lat: garageLatitue, lng: garageLongitude };
-    
-    
-    window.initMap = function () {
-    map = new google.maps.Map(mapDiv, {
-        center: myLatLng,
-        zoom: 14,
-        });
+let map;
 
-    new google.maps.Marker({
-        position: myLatLng,
-        map,
-        title: "Twój warsztat",
-        });
-    }
-});
+const myLatLng = { lat: garageLatitue, lng: garageLongitude };
+
+
+window.initMap = function () {
+map = new google.maps.Map(mapDiv, {
+    center: myLatLng,
+    zoom: 14,
+    });
+
+new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Twój warsztat",
+    });
+}
+
