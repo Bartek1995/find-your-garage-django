1d4bd676991291432320665be845117a1c778a1a
Bartek1995 <bartek.swatek@wp.pl>
2022-11-26 09:55:24 +0100

    Add url for 'ServiceListUpdateView'

--- garages/urls.py
+++ garages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GarageCreateView, GarageEditView, GarageDeleteView, GarageInformationView
+from .views import GarageCreateView, GarageEditView, GarageDeleteView, GarageInformationView, ServiceListUpdateView
 
 
 urlpatterns = [
     path('create/', GarageCreateView.as_view(), name='garage_create'),
     path('edit/<int:garage_id>', GarageEditView.as_view(), name='garage_edit'),
+    path('edit/services/<int:garage_id>', ServiceListUpdateView.as_view(), name='garage_services_edit'),
     path('delete/<int:garage_id>', GarageDeleteView.as_view(), name='garage_delete'),
     path('information/<int:pk>', GarageInformationView.as_view(), name='garage_information'),
 ]
