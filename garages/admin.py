5faab9eba077c66eafe6a62c6d88edc976461d90
Bartek1995 <bartek.swatek@wp.pl>
2022-11-29 16:04:38 +0100

    Add 'OpeningHours' model to admin panel

--- garages/admin.py
+++ garages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Garage, ServiceList
+from .models import Garage, ServiceList, OpeningHours
 
 admin.site.register(Garage)
 admin.site.register(ServiceList)
+admin.site.register(OpeningHours)
