cda0aefe9346682f2036ca61d95ef8f59f2f1319
Bartek1995 <bartek.swatek@wp.pl>
2022-11-29 16:15:22 +0100

    Add view used to edit opening hours to app urls

--- garages/urls.py
+++ garages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import GarageCreateView, GarageEditView, GarageDeleteView, GarageInformationView, ServiceListUpdateView
+from .views import GarageCreateView, GarageEditView, GarageDeleteView, GarageInformationView, ServiceListUpdateView, GarageEditOpeningHours
 
 
 urlpatterns = [
     path('create/', GarageCreateView.as_view(), name='garage_create'),
     path('edit/<int:garage_id>', GarageEditView.as_view(), name='garage_edit'),
     path('edit/services/<int:garage_id>', ServiceListUpdateView.as_view(), name='garage_services_edit'),
+    path('edit/opening_hours/<int:garage_id>', GarageEditOpeningHours.as_view(), name='garage_opening_hours_edit'),
     path('delete/<int:garage_id>', GarageDeleteView.as_view(), name='garage_delete'),
     path('information/<int:pk>', GarageInformationView.as_view(), name='garage_information'),
 ]
