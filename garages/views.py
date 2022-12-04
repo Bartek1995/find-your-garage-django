fb1dc22fe83c89244e8d72121082565db45a0fe2
Bartek1995 <bartek.swatek@wp.pl>
2022-12-04 21:16:52 +0100

    Create view to search garages

--- garages/views.py
+++ garages/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
+from django.core import serializers
 
 from accounts.mixins import GroupRequiredMixin
 from .forms import GarageForm, GarageEditForm, ServiceListEditForm, GarageEditOpeningHoursForm
@@ -185,3 +187,17 @@ class GarageEditOpeningHours(LoginRequiredMixin, GroupRequiredMixin, ModelFormSe
     def formset_valid(self, formset):
         self.messages = messages.success(self.request, 'Godziny otwarcia zostały zaktualizowane.')
         return super().formset_valid(formset)
+    
+    
+class FindGarageView(LoginRequiredMixin, ListView):
+    template_name = 'garages/find_garage.html'
+    model = Garage
+    
+    def get_queryset(self):
+        return Garage.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['map_api_key'] = os.environ.get('GOOGLE_API_KEY')
+        context['garages'] = serializers.serialize('json', self.get_queryset())
+        return context
