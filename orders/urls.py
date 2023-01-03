e1556b699f98ef6b1b1f4c76ee110505df171029
Bartłomiej Swatek <bartek.swatek@wp.pl>
2023-01-03 18:54:39 +0100

    Add history of orders view to app urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateOrderView, SelectDateView
+from .views import CreateOrderView, SelectDateView, HistoryOfOrdersView
 
 
 urlpatterns = [
     path('create/<int:garage_id>/<int:year>/<int:month>/<int:day>/<str:hour>', CreateOrderView.as_view(), name='create_order'),
     path('select_date/<int:garage_id>', SelectDateView.as_view(), name='select_date'),
+    path('history', HistoryOfOrdersView.as_view(), name='history_of_orders'),
 
 ]
